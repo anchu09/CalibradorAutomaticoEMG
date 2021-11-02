3ee30215de5784f5142c8f1212f73ddbc2264ed8
anchu09 <d.anchuela@usp.ceu.es>
2021-11-02 23:42:07 +0100

    comparación entre métodos de preprocesamiento y lavado de cara al código

--- GaussianMixtureM.py
+++ GaussianMixtureM.py
@@ -30,12 +30,12 @@ for x in listaDatos:
     from sklearn import preprocessing
     #normalizamos
     
-    x= preprocessing.normalize(x,axis=0)
-  
+    #x= preprocessing.normalize(x,axis=0)
+    x = preprocessing.minmax_scale(x)
     
     
     #gaussianizamos
-    x=np.log(0.01+x)#ponemos el 0.0001+ para evitar los 0 porque con el log se nos va a inf
+    x=np.log(0.0001+x)#ponemos el 0.0001+ para evitar los 0 porque con el log se nos va a inf
     
     #aplicamos reducción de dimensionalidad
     pca= PCA(n_components=1)
@@ -65,8 +65,10 @@ for x in listaDatos:
     
     db_index = davies_bouldin_score(x, predicciones)
     print("fiabilidad",db_index)
-    plt.show()
+    plt.plot(np.arange(x.shape[0]), x[:, 0])
+
     plt.scatter(np.arange(x.shape[0]), x[:, 0],c=colores_cluster)
+    plt.show()
     listasobreajustada.append(db_index)
 
 #separando datos de prueba y de entrenamiento para evitar el overfitting
@@ -80,26 +82,25 @@ for x in listaDatos:
     from sklearn import preprocessing
     #normalizamos
     
-    x= preprocessing.normalize(x,axis=0)
+    x = preprocessing.minmax_scale(x)
    
-    
-    
     #gaussianizamos
-    x=np.log(1+x)#ponemos el 0.0001+ para evitar los 0 porque con el log se nos va a inf
+    x=np.log(0.0000001+x)#ponemos el 0.0001+ para evitar los 0 porque con el log se nos va a inf
     
     #aplicamos reducción de dimensionalidad
     pca= PCA(n_components=1)
-    pca.fit(x)
-    
-    x=pca.transform(x)
+ 
     from sklearn.model_selection import train_test_split
-    X_train, xtest = train_test_split(x,test_size=0.5)#importante el orden para que no de errores
+    X_train, xtest = train_test_split(x,test_size=0.3)#importante el orden para que no de errores
 
     
     #creamos el gaussian mixture model
+    pca.fit(X_train)
     gmm= GaussianMixture(n_components=2, covariance_type="full", n_init=100)
+    X_train = pca.fit_transform(X_train)
     gmm.fit(X_train)
     
+    xtest = pca.transform(xtest)
     predicciones=gmm.predict(xtest)
     
    # for i, pred in enumerate(predicciones):
@@ -116,7 +117,38 @@ for x in listaDatos:
     from sklearn.metrics import davies_bouldin_score
     
     db_index = davies_bouldin_score(xtest, predicciones)
+    import seaborn as sns
     print("fiabilidad",db_index)
+    #sns.kdeplot(xtest[np.array(colores_cluster) == "red", ].flatten(), bw=0.5)
+    #sns.kdeplot(xtest[np.array(colores_cluster) == "blue", ].flatten(), bw=0.5)
+    plt.hist(xtest[np.array(colores_cluster) == "red", ].flatten())
+    plt.hist(xtest[np.array(colores_cluster) == "blue", ].flatten())
     plt.show()
+    
+    plt.figure()
+    plt.plot(np.arange(xtest.shape[0]), xtest[:, 0])
+
     plt.scatter(np.arange(xtest.shape[0]), xtest[:, 0],c=colores_cluster)
+    plt.show()
+
     listaresultado.append(db_index)
+    
+    
+    
+    
+""" 
+   tabla_Rendimiento = pd.DataFrame()
+tabla_Rendimiento['Set','No preproces','Gauss','Norm','Red_dim','Gauss+Norm','Gauss+red_dim','Norm+Red_dim','Gauss+Norm+Red_dim'] = None
+nueva_Fila={'Set':'x1', 
+            'No preproces': puntuacion(x1, False, False, False),
+            'Gauss':puntuacion(x1, True, False, False),
+            'Norm':puntuacion(x1, False, True, False),
+            'Red_dim':puntuacion(x1, False, False, True),
+            'Gauss+Norm':puntuacion(x1, True, True, False),
+            'Gauss+red_dim':puntuacion(x1, True, False, True),
+            'Norm+Red_dim':puntuacion(x1, False, True, True),
+            'Gauss+Norm+Red_dim':puntuacion(x1, True, True, True)}
+tabla_Rendimiento=tabla_Rendimiento.append(nueva_Fila,ignore_index=True)
+print(tabla_Rendimiento)
+
+"""
--- gausianmixturemodel.py
+++ gausianmixturemodel.py
@@ -1,12 +1,119 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 30 18:35:19 2021
+Created on Sun Oct 10 13:05:22 2021
 
 @author: Usuario
 """
 
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import davies_bouldin_score
 
 
-xi = np.loadtxt("prueba1_features.txt")
+x1= np.loadtxt("prueba1_features.txt")
+x2= np.loadtxt("laura_features.txt")
+x3= np.loadtxt("papa_features.txt")
+x4= np.loadtxt("alex_features.txt")
+x5= np.loadtxt("dani_features.txt")
+
+lista_Datos= [x1,x2,x3,x4,x5]
+
+def reescalamiento(datos):
+    return preprocessing.minmax_scale(datos)
+
+def gaussianizar(datos):
+    return np.log(0.0000001+datos)
+
+def puntuacion(x,reescalar, gaus, red_dim,t_size):
+    if (reescalar==True):
+        x = reescalamiento(x)
+        
+    if (gaus==True):
+        x = gaussianizar(x) 
+  
+    if (red_dim==True):
+        
+        pca= PCA(n_components=1)
+    
+        X_train, X_test = train_test_split(x,test_size=t_size)
+           
+        gmm= GaussianMixture(n_components=2, covariance_type="full", n_init=100)
+        
+        X_train = pca.fit_transform(X_train)
+        
+        gmm.fit(X_train)
+        
+        X_test = pca.transform(X_test)
+    
+    else:
+    
+        X_train, X_test = train_test_split(x,test_size=t_size)
+           
+        gmm= GaussianMixture(n_components=2, covariance_type="full", n_init=100)
+                
+        gmm.fit(X_train)
+        
+        
+    predicciones=gmm.predict(X_test)
+ 
+    colores = ['red','blue']
+    
+    colores_cluster = [colores[predicciones[i]] for i in range(len(X_test))]
+     
+    plt.show()  
+        
+    db_index = davies_bouldin_score(X_test, predicciones)
+   
+    plt.scatter(np.arange(X_test.shape[0]), X_test[:, 0],c=colores_cluster)
+    
+    plt.show()
+
+    return db_index
+
+
+datos_Comparacion = {'No_Prep': puntuacion(x1, False, False, False,0.3),
+            'Ga':puntuacion(x1, True, False, False,0.3),
+            'Nor':puntuacion(x1, False, True, False,0.3),
+            'R_d':puntuacion(x1, False, False, True,0.3),
+            'G+N':puntuacion(x1, True, True, False,0.3),
+            'G+r_d':puntuacion(x1, True, False, True,0.3),
+            'N+r_d':puntuacion(x1, False, True, True,0.3),
+            'G+N+R_d':puntuacion(x1, True, True, True,0.3)}
+
+plt.bar(datos_Comparacion.keys(), datos_Comparacion.values())
+#observaciones: lo que mejor funciona es normalizar y reducir la dimensionalidad = N+r_d
+#pero esta opción no funciona en algunos sets de datos como el 2 -> puntuacion(x2, False, True, True) PETA
+#aunque G+N+R_d es peor pero como la diferencia es pequeña, cogemos la opcion puntuacion(x2, True, True, True) 
+
+
+#para todas las muestras:
+lista_Resultados=[]
+for x in lista_Datos:
+ lista_Resultados.append(puntuacion(x, True, True, True,0.3)) 
+
+
+
+
+
+
+#test: quiero ver con qué tamaño funciona mejor
+
+#lista_x1=[]
+#lista_x2=[]
+#lista_x3=[]
+#for i in range(9):
+   # lista_x1.append(puntuacion(x1, True, True, True,i/10+0.1)) 
+   # lista_x2.append(puntuacion(x1, True, True, True,i/10+0.1)) 
+    #lista_x3.append(puntuacion(x1, True, True, True,i/10+0.1)) 
+    
+#plt.plot(lista_x1)
+#plt.plot(lista_x2)
+#plt.plot(lista_x3)
+
+#concluision: con ninguno en especial porque el score depende del conjunto
+#de datos que es aleatorio. así que lo dejo en 0.3 
+  
